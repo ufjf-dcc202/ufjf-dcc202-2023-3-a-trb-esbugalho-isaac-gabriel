8c2d1eae324274c556c6440ec9b3fe73baa5ec95
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2023-12-09 19:10:36 -0300

    add estilo ao site

--- Trabalho final/js/index.js
+++ Trabalho final/js/index.js
@@ -1,6 +1,5 @@
-// Função para criar um tabuleiro vazio de 3x3
+// Função para criar um tabuleiro vazio 3x3
 function criarTabuleiro() {
-  // Inicializa um array 3x3 preenchido com valores nulos
   const tabuleiro = [
     [null, null, null],
     [null, null, null],
@@ -9,56 +8,72 @@ function criarTabuleiro() {
   return tabuleiro;
 }
 
-// Função para renderizar o tabuleiro na interface
+// Função para renderizar o tabuleiro na página HTML
 function renderizarTabuleiro(tabuleiro, jogadorId) {
-  // Obtém o elemento HTML do tabuleiro do jogador
+  // Obter o elemento do tabuleiro com base no jogadorId
   const tabuleiroElemento = document.getElementById(`jogador${jogadorId}-tabuleiro`);
-  // Limpa o conteúdo do elemento
+  // Limpar o conteúdo atual do elemento
   tabuleiroElemento.innerHTML = '';
 
-  // Loop para percorrer as linhas do tabuleiro
+  // Iterar sobre as linhas do tabuleiro
   for (let i = 0; i < tabuleiro.length; i++) {
-    // Cria um elemento de linha para cada linha do tabuleiro
+    // Criar um elemento div para representar uma linha
     const linhaElemento = document.createElement('div');
     linhaElemento.className = 'linha';
 
-    // Loop para percorrer as colunas do tabuleiro
+    // Iterar sobre as colunas da linha
     for (let j = 0; j < tabuleiro[i].length; j++) {
-      // Cria um elemento de célula para cada célula do tabuleiro
+      // Criar um elemento div para representar uma célula
       const celulaElemento = document.createElement('div');
       celulaElemento.className = 'celula';
-      
-      // Define o texto da célula com o valor do tabuleiro ou uma string vazia se for nulo
+
+      // Atribuir o texto da célula com o valor presente no tabuleiro ou uma string vazia se for nulo
       celulaElemento.innerText = tabuleiro[i][j] || '';
-      
-      // Adiciona um evento de clique para lidar com a interação do jogador
+
+      // Associar a função Clique ao evento de clique da célula
       celulaElemento.onclick = () => Clique(jogadorId, i, j);
-      
-      // Adiciona a célula à linha
+
+      // Adicionar a célula à linha
       linhaElemento.appendChild(celulaElemento);
     }
 
-    // Adiciona a linha ao tabuleiro
+    // Adicionar a linha ao tabuleiro
     tabuleiroElemento.appendChild(linhaElemento);
   }
 }
 
-// Função para lidar com o clique do jogador em uma célula
+// Função chamada quando uma célula é clicada
 function Clique(jogadorId, linha, coluna) {
-  // Obtém o tabuleiro do jogador atual
+  // Obter o tabuleiro atual e o tabuleiro do jogador adversário
   const tabuleiroAtual = jogadorId === 1 ? jogador1Tabuleiro : jogador2Tabuleiro;
+  const outroTabuleiro = jogadorId === 1 ? jogador2Tabuleiro : jogador1Tabuleiro;
 
-  // Se a célula estiver vazia, preenche com um número aleatório de 1 a 9
+  // Verificar se a célula está vazia
   if (!tabuleiroAtual[linha][coluna]) {
-    tabuleiroAtual[linha][coluna] = Math.floor(Math.random() * 9) + 1;
+    // Gerar um número aleatório de 1 a 9 e atribuir à célula
+    const numeroAleatorio = Math.floor(Math.random() * 9) + 1;
+    tabuleiroAtual[linha][coluna] = numeroAleatorio;
+
+    // Verificar se existe um número igual na linha do outro jogador
+    for (let i = 0; i < outroTabuleiro.length; i++) {
+      const linhaAdversario = outroTabuleiro[i];
+      // Se houver um número igual, zerar toda a linha do adversário
+      if (linhaAdversario.includes(numeroAleatorio)) {
+        for (let j = 0; j < linhaAdversario.length; j++) {
+          linhaAdversario[j] = 0;
+        }
+      }
+    }
   }
 
-  // Atualiza a renderização do tabuleiro na interface
+  // Renderizar o tabuleiro atualizado na página
   renderizarTabuleiro(tabuleiroAtual, jogadorId);
 }
 
-// Inicialização dos tabuleiros e renderização inicial na interface
+// Criar os tabuleiros para os dois jogadores
 let jogador1Tabuleiro = criarTabuleiro();
 let jogador2Tabuleiro = criarTabuleiro();
+
+// Inicialmente, renderizar os tabuleiros na página
 renderizarTabuleiro(jogador1Tabuleiro, 1);
 renderizarTabuleiro(jogador2Tabuleiro, 2);
