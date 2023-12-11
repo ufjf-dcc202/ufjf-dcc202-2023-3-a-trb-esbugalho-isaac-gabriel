e258fc4fdc3c80a7e686dd3b4d795e0ac14be5c7
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2023-12-11 00:10:26 -0300

    add comentarios explicativos no codigo js

--- Trabalho final/js/index.js
+++ Trabalho final/js/index.js
@@ -1,3 +1,4 @@
+// Função para criar um novo tabuleiro com valores nulos
 function criarTabuleiro() {
   const tabuleiro = [
     [null, null, null],
@@ -27,57 +28,92 @@ function renderizarTabuleiro(tabuleiro, jogadorId) {
       celulaElemento.className = 'celula';
 
       // Atribuir o texto da célula com o valor presente no tabuleiro ou uma string vazia se for nulo
-      celulaElemento.innerText = tabuleiro[i][j] || '';
+      celulaElemento.innerText = tabuleiro[i][j];
       
       // Associar a função Clique ao evento de clique da célula
-      celulaElemento.onclick = () => Clique(jogadorId, i, j);
+      celulaElemento.onclick = function() {
+        Clique(jogadorId, i, j);
+      };
 
       // Adicionar a célula à linha
-      linhaElemento.appendChild(celulaElemento);
+      linhaElemento.appendChild(celulaElemento); 
     }
 
     // Adicionar a linha ao tabuleiro
     tabuleiroElemento.appendChild(linhaElemento);
   }
 }
+
+// Função para calcular a pontuação total do tabuleiro
+function calcularPontuacao(tabuleiro) {
+  let pontuacao = 0;
+  for (let i = 0; i < tabuleiro.length; i++) {
+    for (let j = 0; j < tabuleiro[i].length; j++) {
+      pontuacao += tabuleiro[i][j];
+    }
+  }
+  return pontuacao;
+}
+
+// Função para verificar se um jogador atingiu a pontuação máxima
+function verificarVencedor(tabuleiro, jogadorId, jogadas) {
+  if (jogadas === 6) {
+    const pontuacaoAtual = calcularPontuacao(tabuleiro);
+    alert('Jogador ' + jogadorId + ' venceu com ' + pontuacaoAtual + ' pontos.');
+  }
+}
+
+// Função chamada quando uma célula é clicada
 function Clique(jogadorId, linha, coluna) {
   let tabuleiroAtual;
   let outroTabuleiro;
 
+  // Determinar qual tabuleiro e jogadas pertencem ao jogador atual e ao adversário
   if (jogadorId === 1) {
     tabuleiroAtual = jogador1Tabuleiro;
     outroTabuleiro = jogador2Tabuleiro;
+    jogador1Jogadas++;
   } else {
     tabuleiroAtual = jogador2Tabuleiro;
     outroTabuleiro = jogador1Tabuleiro;
+    jogador2Jogadas++;
   }
 
+  // Verificar se a célula está vazia
   if (!tabuleiroAtual[linha][coluna]) {
+    // Gerar um número aleatório e atribuir à célula atual
     const numeroAleatorio = Math.floor(Math.random() * 9) + 1;
     tabuleiroAtual[linha][coluna] = numeroAleatorio;
-
+ 
     // Verificar se pelo menos um valor na linha do adversário é igual ao valor clicado
-    const temValorIgual = outroTabuleiro[linha].some((numero) => numero === numeroAleatorio);
+    const ValorIgual = outroTabuleiro[linha].includes(numeroAleatorio);
 
-    if (temValorIgual) {
-      // Se pelo menos um valor for igual, zera a linha do adversário
+
+    // Se pelo menos um valor for igual, zera a linha do adversário
+    if (ValorIgual) {
       for (let j = 0; j < outroTabuleiro[linha].length; j++) {
         outroTabuleiro[linha][j] = 0;
       }
     }
   }
 
+  // Atualizar a representação visual do tabuleiro
   renderizarTabuleiro(tabuleiroAtual, jogadorId);
+
+  // Verificar se há um vencedor
+  if (jogadorId === 1) {
+    verificarVencedor(tabuleiroAtual, jogadorId, jogador1Jogadas);
+  } else {
+    verificarVencedor(tabuleiroAtual, jogadorId, jogador2Jogadas);
+  }
 }
 
-
+// Inicializar os tabuleiros e contadores de jogadas
 let jogador1Tabuleiro = criarTabuleiro();
 let jogador2Tabuleiro = criarTabuleiro();
+let jogador1Jogadas = 0;
+let jogador2Jogadas = 0;
 
+// Renderizar os tabuleiros na página
 renderizarTabuleiro(jogador1Tabuleiro, 1);
 renderizarTabuleiro(jogador2Tabuleiro, 2);
-
-
-
-
-
