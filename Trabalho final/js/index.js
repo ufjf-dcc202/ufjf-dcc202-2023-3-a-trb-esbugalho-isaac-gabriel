d6b56a8c268ed484583a086aaab1ed0ac00bd65f
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2023-12-12 00:10:08 -0300

    criando funcao clique e chamandoa funcao verificar e rendenizar

--- Trabalho final/js/index.js
+++ Trabalho final/js/index.js
@@ -47,3 +47,41 @@ function verificarVencedor(tabuleiro, jogadorId, jogadas) {
     alert('Jogador ' + jogadorId + ' venceu com ' + pontuacaoAtual + ' pontos.');
   }
 }
+
+function Clique(jogadorId, linha, coluna) {
+  let tabuleiroAtual;
+  let outroTabuleiro;
+
+  if (jogadorId === 1) {
+    tabuleiroAtual = jogador1Tabuleiro;
+    outroTabuleiro = jogador2Tabuleiro;
+    jogador1Jogadas++;
+  } else {
+    tabuleiroAtual = jogador2Tabuleiro;
+    outroTabuleiro = jogador1Tabuleiro;
+    jogador2Jogadas++;
+  }
+
+  if (!tabuleiroAtual[linha][coluna]) {
+    const numeroAleatorio = Math.floor(Math.random() * 9) + 1;
+    tabuleiroAtual[linha][coluna] = numeroAleatorio;
+
+    const ValorIgual = outroTabuleiro[linha].includes(numeroAleatorio);
+
+    if (ValorIgual) {
+      for (let j = 0; j < outroTabuleiro[linha].length; j++) {
+        outroTabuleiro[linha][j] = 0;
+      }
+    }
+
+    audio.play();
+  }
+
+  renderizarTabuleiro(tabuleiroAtual, jogadorId);
+
+  if (jogadorId === 1) {
+    verificarVencedor(tabuleiroAtual, jogadorId, jogador1Jogadas);
+  } else {
+    verificarVencedor(tabuleiroAtual, jogadorId, jogador2Jogadas);
+  }
+}
