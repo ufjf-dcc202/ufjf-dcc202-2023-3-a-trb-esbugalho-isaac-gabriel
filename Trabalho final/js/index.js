44a41d885d7ae89826a3d64743f3f3c883e2e9f0
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2023-12-12 00:06:03 -0300

    criando a funçao calcularPontuacao

--- Trabalho final/js/index.js
+++ Trabalho final/js/index.js
@@ -31,3 +31,13 @@ function renderizarTabuleiro(tabuleiro, jogadorId) {
     tabuleiroElemento.appendChild(linhaElemento);
   }
 }
+
+function calcularPontuacao(tabuleiro) {
+  let pontuacao = 0;
+  for (let i = 0; i < tabuleiro.length; i++) {
+    for (let j = 0; j < tabuleiro[i].length; j++) {
+      pontuacao += tabuleiro[i][j];
+    }
+  }
+  return pontuacao;
+}
