c4a8f501abb6945db4b5c7b3baf8dcf44ec32534
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2023-12-12 00:04:59 -0300

    criando a funçao renderizarTabuleiro

--- Trabalho final/js/index.js
+++ Trabalho final/js/index.js
@@ -6,3 +6,28 @@ function criarTabuleiro() {
   ];
   return tabuleiro;
 }
+
+function renderizarTabuleiro(tabuleiro, jogadorId) {
+  const tabuleiroElemento = document.getElementById(`jogador${jogadorId}-tabuleiro`);
+  tabuleiroElemento.innerHTML = '';
+
+  for (let i = 0; i < tabuleiro.length; i++) {
+    const linhaElemento = document.createElement('div');
+    linhaElemento.className = 'linha';
+
+    for (let j = 0; j < tabuleiro[i].length; j++) {
+      const celulaElemento = document.createElement('div');
+      celulaElemento.className = 'celula';
+
+      celulaElemento.innerText = tabuleiro[i][j] || '';
+
+      celulaElemento.onclick = function () {
+        Clique(jogadorId, i, j);
+      };
+
+      linhaElemento.appendChild(celulaElemento);
+    }
+
+    tabuleiroElemento.appendChild(linhaElemento);
+  }
+}
