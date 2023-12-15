f842c1d39fea1fd16e47102158889fdd2e14f079
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2023-12-15 12:10:10 -0300

    add comentarios explicativos na codigo js

--- Trabalho-final/js/interface.js
+++ Trabalho-final/js/interface.js
@@ -1,35 +1,49 @@
-
-
+// Importa a função 'Clique' do arquivo 'jogo.js'
 import { Clique } from './jogo.js';
 
-function renderizarTabuleiro(tabuleiro, jogadorId, outroTabuleiro, audio) {
+// Função para renderizar o tabuleiro na interface do usuário
+export function renderizarTabuleiro(tabuleiro, jogadorId, outroTabuleiro, audio) {
+  // Obtém o elemento HTML onde o tabuleiro será renderizado
   const tabuleiroElemento = document.getElementById('jogador' + jogadorId + '-tabuleiro');
+
+  // Limpa o conteúdo anterior do elemento para renderizar o novo tabuleiro
   tabuleiroElemento.innerHTML = '';
 
+  // Loop para percorrer cada linha do tabuleiro
   for (let i = 0; i < tabuleiro.length; i++) {
+    // Cria um elemento div para representar uma linha do tabuleiro
     const linhaElemento = document.createElement('div');
     linhaElemento.className = 'linha';
 
+    // Loop para percorrer cada célula da linha do tabuleiro
     for (let j = 0; j < tabuleiro[i].length; j++) {
+      // Cria um elemento div para representar uma célula do tabuleiro
       const celulaElemento = document.createElement('div');
       celulaElemento.className = 'celula';
 
+      // Obtém o número presente na célula
       const numero = tabuleiro[i][j];
+
+      // Cria um elemento span para exibir o número na célula (ou vazio se for null)
       const numeroElemento = document.createElement('span');
       numeroElemento.innerText = numero !== null ? numero : '';
 
+      // Adiciona o elemento de número à célula
       celulaElemento.appendChild(numeroElemento);
 
+      // Adiciona um evento de clique à célula
       celulaElemento.onclick = function () {
+        // Chama a função 'Clique' do arquivo 'jogo.js' ao clicar na célula
         Clique(jogadorId, i, j, tabuleiro, outroTabuleiro, audio);
+        // Após o clique, renderiza novamente o tabuleiro
         renderizarTabuleiro(tabuleiro, jogadorId, outroTabuleiro, audio);
       };
 
+      // Adiciona a célula à linha do tabuleiro
       linhaElemento.appendChild(celulaElemento);
     }
 
+    // Adiciona a linha completa ao elemento do tabuleiro
     tabuleiroElemento.appendChild(linhaElemento);
   }
 }
-
-export { renderizarTabuleiro };
--- Trabalho-final/js/jogo.js
+++ Trabalho-final/js/jogo.js
@@ -1,7 +1,5 @@
-export { criarTabuleiro, Clique, verificarVencedor, contarJogadas, calcularPontuacao };
-
-
-function criarTabuleiro() {
+// Função para criar um novo tabuleiro vazio
+export function criarTabuleiro() {
   const tabuleiro = [
     [null, null, null],
     [null, null, null],
@@ -10,25 +8,31 @@ function criarTabuleiro() {
   return tabuleiro;
 }
 
-function Clique(jogadorId, linha, coluna, tabuleiroAtual, outroTabuleiro, audio) {
-  
-  if (!tabuleiroAtual[linha][coluna]) {
+// Função chamada quando um jogador realiza um clique no tabuleiro
+export function Clique(jogadorId, linha, coluna, tabuleiroAtual, outroTabuleiro, audio) {
+  // Verifica se a célula clicada está vazia
+  if (tabuleiroAtual && tabuleiroAtual[linha] && tabuleiroAtual[linha][coluna] === null) {
+    // Gera um número aleatório para preencher a célula clicada
     const numeroAleatorio = Math.floor(Math.random() * 9) + 1;
     tabuleiroAtual[linha][coluna] = numeroAleatorio;
 
-    const valorIgual = outroTabuleiro[linha].includes(numeroAleatorio);
-
-    if (valorIgual) {
+    // Verifica se o número aleatório está presente no tabuleiro do adversário
+    if (outroTabuleiro && outroTabuleiro[linha] && outroTabuleiro[linha].includes(numeroAleatorio)) {
+      // Se presente, limpa essa célula no tabuleiro do adversário
       for (let j = 0; j < outroTabuleiro[linha].length; j++) {
         outroTabuleiro[linha][j] = null;
       }
     }
 
-    audio.play();
+    // Reproduz o áudio (se disponível)
+    if (audio && typeof audio.play === 'function') {
+      audio.play();
+    }
   }
 }
 
-function calcularPontuacao(tabuleiro) {
+// Função para calcular a pontuação total do tabuleiro
+export function calcularPontuacao(tabuleiro) {
   let pontuacao = 0;
   for (let i = 0; i < tabuleiro.length; i++) {
     for (let j = 0; j < tabuleiro[i].length; j++) {
@@ -38,7 +42,8 @@ function calcularPontuacao(tabuleiro) {
   return pontuacao;
 }
 
-function contarJogadas(tabuleiro) {
+// Função para contar o número de jogadas realizadas no tabuleiro
+export function contarJogadas(tabuleiro) {
   let jogadas = 0;
   for (let i = 0; i < tabuleiro.length; i++) {
     for (let j = 0; j < tabuleiro[i].length; j++) {
@@ -50,12 +55,17 @@ function contarJogadas(tabuleiro) {
   return jogadas;
 }
 
-function verificarVencedor(tabuleiro, jogadorId) {
-   jogadas = contarJogadas(tabuleiro);
+// Função para verificar se um jogador venceu (por exemplo, ao completar 6 jogadas)
+export function verificarVencedor(tabuleiro, jogadorId) {
+  // Conta o número total de jogadas no tabuleiro
+  let jogadas = contarJogadas(tabuleiro);
+
+  // Verifica se o jogador atingiu a condição de vitória (por exemplo, 6 jogadas)
   if (jogadas === 6) {
+    // Calcula a pontuação atual do jogador
     const pontuacaoAtual = calcularPontuacao(tabuleiro);
+
+    // Exibe um alerta indicando que o jogador venceu com a pontuação obtida
     alert('Jogador ' + jogadorId + ' venceu com ' + pontuacaoAtual + ' pontos.');
   }
 }
-
-
