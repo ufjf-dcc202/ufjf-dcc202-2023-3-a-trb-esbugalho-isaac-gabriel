2304386ff6339d38997be470a9dd3e3568a28da4
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2023-12-15 14:08:50 -0300

    ultimos detalhes

--- Trabalho-final/js/index.js
+++ Trabalho-final/js/index.js
@@ -1,51 +1,33 @@
-// Importa funções do arquivo 'jogo.js' e 'interface.js'
 import { criarTabuleiro, Clique, verificarVencedor } from './jogo.js';
 import { renderizarTabuleiro } from './interface.js';
 
-// Inicializa um objeto de áudio com um arquivo de áudio específico
 var audio = new Audio('js/audio_file.mp3');
 
-// Inicializa tabuleiros para jogadores 1 e 2
 var jogador1Tabuleiro = criarTabuleiro();
 var jogador2Tabuleiro = criarTabuleiro();
 
-// Renderiza os tabuleiros na interface
 renderizarTabuleiro(jogador1Tabuleiro, 1, jogador2Tabuleiro, audio);
 renderizarTabuleiro(jogador2Tabuleiro, 2, jogador1Tabuleiro, audio);
 
-// Adiciona evento de clique para o tabuleiro do jogador 1
 document.getElementById('jogador1-tabuleiro').onclick = function () {
-  // Chama a função 'Clique' para o jogador 1
   Clique(1, jogador1Tabuleiro, jogador2Tabuleiro, audio);
-  // Após o clique, renderiza novamente o tabuleiro e verifica se há um vencedor
   renderizarTabuleiro(jogador1Tabuleiro, 1, jogador2Tabuleiro, audio);
   verificarVencedor(jogador1Tabuleiro, 1);
 };
 
-// Adiciona evento de clique para o tabuleiro do jogador 2
 document.getElementById('jogador2-tabuleiro').onclick = function () {
-  // Chama a função 'Clique' para o jogador 2
   Clique(2, jogador2Tabuleiro, jogador1Tabuleiro, audio);
-  // Após o clique, renderiza novamente o tabuleiro e verifica se há um vencedor
   renderizarTabuleiro(jogador2Tabuleiro, 2, jogador1Tabuleiro, audio);
   verificarVencedor(jogador2Tabuleiro, 2);
 };
 
-// Adiciona evento de clique para o botão de reinício
 document.getElementById('Reiniciar').onclick = function () {
-  // Chama a função para reiniciar o jogo
   reiniciarJogo();
 };
 
-
-
-// Função para reiniciar o jogo
 function reiniciarJogo() {
-  // Cria novos tabuleiros para jogadores 1 e 2
   jogador1Tabuleiro = criarTabuleiro();
   jogador2Tabuleiro = criarTabuleiro();
-
-  // Renderiza os tabuleiros reiniciados na interface
   renderizarTabuleiro(jogador1Tabuleiro, 1, jogador2Tabuleiro, audio);
   renderizarTabuleiro(jogador2Tabuleiro, 2, jogador1Tabuleiro, audio);
 }
--- Trabalho-final/js/interface.js
+++ Trabalho-final/js/interface.js
@@ -1,57 +1,36 @@
-// Importa a função 'Clique' do arquivo 'jogo.js'
 import { Clique } from './jogo.js';
 
-// Função para renderizar o tabuleiro na interface do usuário
 export function renderizarTabuleiro(tabuleiro, jogadorId, outroTabuleiro, audio) {
-  // Obtém o elemento HTML onde o tabuleiro será renderizado
   const tabuleiroElemento = document.getElementById('jogador' + jogadorId + '-tabuleiro');
-
-  // Limpa o conteúdo anterior do elemento para renderizar o novo tabuleiro
   tabuleiroElemento.innerHTML = '';
 
-  // Loop para percorrer cada linha do tabuleiro
   for (let i = 0; i < tabuleiro.length; i++) {
-    // Cria um elemento div para representar uma linha do tabuleiro
     const linhaElemento = document.createElement('div');
     linhaElemento.className = 'linha';
 
-    // Loop para percorrer cada célula da linha do tabuleiro
     for (let j = 0; j < tabuleiro[i].length; j++) {
-      // Cria um elemento div para representar uma célula do tabuleiro
       const celulaElemento = document.createElement('div');
       celulaElemento.className = 'celula';
 
-      // Obtém o número presente na célula
       const numero = tabuleiro[i][j];
-
-      // Cria um novo elemento 'span'
       const numeroElemento = document.createElement('span');
 
-      // Verifica se o valor de 'numero' não é nulo
       if (numero !== null) {
-        // Se 'numero' não for nulo, define o texto do elemento como o valor de 'numero'
         numeroElemento.innerText = numero;
       } else {
-        // Se 'numero' for nulo, define o texto do elemento como uma string vazia
         numeroElemento.innerText = '';
       }
 
-      // Adiciona o elemento de número à célula
       celulaElemento.appendChild(numeroElemento);
 
-      // Adiciona um evento de clique à célula
       celulaElemento.onclick = function () {
-        // Chama a função 'Clique' do arquivo 'jogo.js' ao clicar na célula
-        Clique(jogadorId, i, j, tabuleiro, outroTabuleiro, audio);
-        // Após o clique, renderiza novamente o tabuleiro
+        Clique(jogadorId, i, j, tabuleiro, audio);
         renderizarTabuleiro(tabuleiro, jogadorId, outroTabuleiro, audio);
       };
 
-      // Adiciona a célula à linha do tabuleiro
       linhaElemento.appendChild(celulaElemento);
     }
 
-    // Adiciona a linha completa ao elemento do tabuleiro
     tabuleiroElemento.appendChild(linhaElemento);
   }
 }
