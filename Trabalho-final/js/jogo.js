81d2da773f9ba9576d556d535dc960972e77f10d
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2023-12-18 11:17:32 -0300

    arumando a funçao clique

--- Trabalho-final/js/jogo.js
+++ Trabalho-final/js/jogo.js
@@ -1,4 +1,5 @@
-//jogo
+// Importe a função renderizarTabuleiro aqui, se necessário
+
 export function criarTabuleiro() {
   const tabuleiro = [
     [null, null, null],
@@ -10,18 +11,49 @@ export function criarTabuleiro() {
 
 export function Clique(jogadorId, linha, coluna, tabuleiroAtual, outroTabuleiro, audio) {
   if (tabuleiroAtual && tabuleiroAtual[linha] && tabuleiroAtual[linha][coluna] === null) {
-    const numeroAleatorio = Math.floor(Math.random() * 9) + 1;
+    const numeroAleatorio = Math.floor(Math.random() * 6) + 1; 
+
     tabuleiroAtual[linha][coluna] = numeroAleatorio;
 
-    if (outroTabuleiro && outroTabuleiro[linha] && outroTabuleiro[linha].includes(numeroAleatorio)) {
-      for (let j = 0; j < outroTabuleiro[linha].length; j++) {
-        outroTabuleiro[linha][j] = null;
+    // Calcular a pontuação da coluna
+    let pontuacaoColuna = 0;
+    for (let i = 0; i < tabuleiroAtual.length; i++) {
+      pontuacaoColuna += tabuleiroAtual[i][coluna];
+    }
+
+    // Multiplicar pela quantidade de dados de mesmo valor na coluna
+    let quantidadeDados = 0;
+    for (let i = 0; i < tabuleiroAtual.length; i++) {
+      if (tabuleiroAtual[i][coluna] === numeroAleatorio) {
+        quantidadeDados++;
       }
     }
 
+    pontuacaoColuna *= quantidadeDados;
+
+    // Atualizar a pontuação total
+    let pontuacaoTotal = calcularPontuacao(tabuleiroAtual);
+
+    // Atualizar a pontuação do jogador
+    pontuacaoTotal -= pontuacaoColuna;
+    pontuacaoTotal += pontuacaoColuna;
+
+    // Atualizar o tabuleiro do oponente
+    if (outroTabuleiro && outroTabuleiro[linha]) {
+      for (let i = 0; i < outroTabuleiro[linha].length; i++) {
+        if (outroTabuleiro[linha][i] === numeroAleatorio) {
+          outroTabuleiro[linha][i] = null;
+        }
+      }
+    }
+
+    // Reproduzir o áudio se disponível
     if (audio && typeof audio.play === 'function') {
       audio.play();
     }
+
+    // Renderizar os tabuleiros após a atualização
+    renderizarTabuleiro(tabuleiroAtual, jogadorId, outroTabuleiro, audio);
   }
 }
 
@@ -48,8 +80,21 @@ export function contarJogadas(tabuleiro) {
 }
 
 export function verificarVencedor(tabuleiro, jogadorId) {
-  let jogadas = contarJogadas(tabuleiro);
-  if (jogadas === 6) {
+  let todasAsCasasPreenchidas = true;
+
+  for (let i = 0; i < tabuleiro.length; i++) {
+    for (let j = 0; j < tabuleiro[i].length; j++) {
+      if (tabuleiro[i][j] === null) {
+        todasAsCasasPreenchidas = false;
+        break;
+      }
+    }
+    if (!todasAsCasasPreenchidas) {
+      break;
+    }
+  }
+
+  if (todasAsCasasPreenchidas) {
     const pontuacaoAtual = calcularPontuacao(tabuleiro);
     alert('Jogador ' + jogadorId + ' venceu com ' + pontuacaoAtual + ' pontos.');
   }
